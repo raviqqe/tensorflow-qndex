25bf1f54c57496a36e19e9ed56affe7e308299b6
Yota Toyama <raviqqe@gmail.com>
2017-01-12 10:06:45 +0900

    Let classify() set mode

--- qndex/classify.py
+++ qndex/classify.py
@@ -5,21 +5,25 @@ import qnd
 
 def def_classify():
     qnd.add_required_flag("num_classes", type=int)
+    qnd.add_flag("num_labels", type=int, help="Required only for inference")
     qnd.add_flag("hidden_layer_sizes", type=argtyp.int_list, default=[100])
     qnd.add_flag("dropout_keep_prob", type=float)
 
-    def classify(feature, label):
+    def classify(feature, label=None, *, mode):
         if qnd.FLAGS.num_classes <= 1:
             raise ValueError("Number of classes must be greater than 1.")
+
+        num_labels = qnd.FLAGS.num_labels or ex.num_labels(label)
 
         return ex.classify(
             ex.mlp(
                 feature,
                 layer_sizes=[
                     *qnd.FLAGS.hidden_layer_sizes,
-                    ex.num_logits(ex.num_labels(label), qnd.FLAGS.num_classes)],
+                    ex.num_logits(num_labels, qnd.FLAGS.num_classes)],
                 dropout_keep_prob=qnd.FLAGS.dropout_keep_prob),
             label,
-            binary=(qnd.FLAGS.num_classes == 2))
+            num_classes=qnd.FLAGS.num_classes,
+            num_labels=num_labels)
 
     return classify
--- qndex/classify_test.py
+++ qndex/classify_test.py
@@ -4,4 +4,4 @@ from .classify import *
 
 
 def test_def_classify():
-    assert isinstance(def_classify, typing.Callable)
+    assert isinstance(def_classify(), typing.Callable)
